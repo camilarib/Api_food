4738892a7fee6db1771d716cf84973d2d70dcd32
Camila <125229387+camilarib@users.noreply.github.com>
2023-09-26 15:36:54 -0300

    Update Order.ts

--- src/app/models/Order.ts
+++ src/app/models/Order.ts
@@ -1,24 +1,24 @@
-import {model, Schema } from 'mongoose';
+import {model, Schema } from 'mongoose'; // principais classes usadas para definir e criar modelos de dados para o MongoDB
 
-export const Order = model('Order', new Schema({
+export const Order = model('Order', new Schema({ // cria um modelo chamado order
 	table: {
-		type: String,
-		required: true,
+		type: String, //define um campo do tipo string
+		required: true, //define a propriedade required
 	},
 	status: {
-		type: String,
-		enum: ['WAITING', 'IN_PRODUCTION', 'DONE'],
-		default: 'WAITING',
+		type: String, //define um campo do tipo string
+		enum: ['WAITING', 'IN_PRODUCTION', 'DONE'], // propriedade enum especifica que o campo status só pode ter um dos valores permitidos 
+		default: 'WAITING', //valor padrão é 'WAITING'.
 	},
 	creatdAt: {
-		type: Date,
-		default: Date.now,
+		type: Date, //define um campo do tipo date
+		default: Date.now, //valor padrão definido com date.now
 	},
 	products: {
 		required: true,
 		type:[{
 			product: {
-				type: Schema.Types.ObjectId,
+				type: Schema.Types.ObjectId, //referência a um produto
 				required: true,
 				ref: 'Product',
 			},
