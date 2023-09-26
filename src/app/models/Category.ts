fcf7c461ac42cd52ae1d8ee365cb0ccbbf593255
Camila <125229387+camilarib@users.noreply.github.com>
2023-09-26 15:18:49 -0300

    Update Category.ts

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,12 +1,12 @@
-import {model, Schema } from 'mongoose';
+import {model, Schema } from 'mongoose'; // importa as bibliotecas nescessarias 
 
-export const Category = model('Category', new Schema({
+export const Category = model('Category', new Schema({ // define modelos de dados 
 	name: {
-		type: String,
-		required: true,
+		type: String, //define um campo do tipo String
+		required: true, //atribui valor para o campo name 
 	},
 	icon: {
-		type: String,
-		required: true,
+		type: String, //define um campo do tipo String
+		required: true, //atribui valor para o campo icon
 	}
 }));
